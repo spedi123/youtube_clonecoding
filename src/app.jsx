4ab13a01942a5a6e5c027e300f2c793a69d47139
spedi123 <peteran8604@gmail.com>
2021-12-02 12:59:20 -0800

    make video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./app.css";
-import VideoList from "./components/vidoe_list/video_list";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVidoes] = useState([]);
