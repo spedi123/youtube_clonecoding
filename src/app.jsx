c051bab90851186aa0ddaca387d1330be404446a
spedi123 <peteran8604@gmail.com>
2021-12-02 02:01:09 -0800

    set up video item

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,24 @@
+import React, { useEffect, useState } from "react";
 import "./app.css";
+import VideoList from "./components/vidoe_list/video_list";
 
 function App() {
-  return <h1>Hello!!</h1>;
+  const [videos, setVidoes] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyA9YyjplMTo-u0jWPKaLCYUwdGGEJ6XST8",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVidoes(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
 }
 
 export default App;
