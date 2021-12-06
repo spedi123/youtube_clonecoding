cf24849a012ea25f794f957c732074f83704f6dc
spedi123 <peteran8604@gmail.com>
2021-12-05 17:11:10 -0800

    add responsive mode and clean up

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,30 +1,46 @@
+import axios from "axios";
+
 class Youtube {
   constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+    this.youtube = axios.create({
+      baseURL: "https://www.googleapis.com/youtube/v3",
+      params: { key: key },
+    });
   }
 
   async mostPopular() {
-    return fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => result.items);
+    const response = await this.youtube.get("video", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+      },
+    });
+    // const response = await fetch(
+    //   `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+    //   this.getRequestOptions
+    // )
+    //   .then((response) => response.json())
+    //   .then((result) => result.items);
   }
 
   async search(query) {
-    return fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId }))
-      );
+    const response = await this.youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 25,
+        type: "video",
+        q: query,
+      },
+    });
+    // const response = await fetch(
+    //   `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
+    //   this.getRequestOptions
+    // )
+    //   .then((response) => response.json())
+    //   .then((result) =>
+    //     result.items.map((item) => ({ ...item, id: item.id.videoId }))
+    //   );
   }
 }
 
